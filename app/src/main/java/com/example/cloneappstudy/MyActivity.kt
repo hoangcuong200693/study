5dfc9bf1943ca583e24ef16e3cca55b73201b85f
Cuong Hoang <hoangcuong200693@gmail.com>
2023-12-05 21:55:19 +0700

    decode study

---
+++ app/src/main/java/com/example/cloneappstudy/MyActivity.kt
@@ -0,0 +1,10 @@
+package com.example.cloneappstudy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
